e53b05622bd8d46adf7442f6515c67ad9fd56c5f
YoadBarok <74326426+YoadBarok@users.noreply.github.com>
2022-08-11 11:29:05 +0200

    Add url var

--- backend/src/emailTemplates/newUser.js
+++ backend/src/emailTemplates/newUser.js
@@ -2,7 +2,7 @@ let template = (code) => {
 return `
 <h1>Welcome to my Login App!</h1>
 <h2>Please verify your account by clicking the following link:</h2>
-<a href="http://localhost:${process.env.PORT}/user/verify/${code}">Click me!</a>
+<a href="${process.env.URL}/user/verify/${code}">Click me!</a>
 `
 }    
 
