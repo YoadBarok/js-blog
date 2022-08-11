dabfea8ab93a4edb2176578ce9523e6deb57f1c8
YoadBarok <74326426+YoadBarok@users.noreply.github.com>
2022-08-11 11:16:09 +0200

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST || "localhost",
         dialect: "postgres",
 
         pool: {
