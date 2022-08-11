acbd9d8e30815f838d92f3801b084c345b901636
YoadBarok <74326426+YoadBarok@users.noreply.github.com>
2022-08-11 11:14:12 +0200

    change db host

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: "dcmdqkogslkuqp",
+        host: process.env.DB_HOST,
         dialect: "postgres",
 
         pool: {
